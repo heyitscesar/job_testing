4ebcecfae8bc6ac5c0561003fa981955db8d36d2
heyitscesar <cesarhurtado28@gmail.com>
2024-03-21 20:50:20 -0600

    Add files via upload
    Open Tools set

---
+++ openSymbeeTools.js
@@ -0,0 +1,20 @@
+//    "https://symbeeconnect.com/symbee-connect/ui/abgprod/prod",
+
+const urls = [
+    "https://g.loyalty-kognitiv.com/login?errorCode=sessionExpired",
+    "https://ebc.cybersource.com/ebc2/",
+    "https://callcentre-loyalty.abg.loyalty-kognitiv.com/login?errorCode=sessionExpired",
+    "https://tellmeapp.azurewebsites.net/Auth/Login?",
+    "https://www.abgvoice.com/xvr/login.mvc",
+    "https://chromewebstore.google.com/detail/text-blaze-templates-and/idgadaccgipmpannjkmfddolnnhmeklj?pli=1",
+];
+
+// Function to open each URL in a new tab
+function openTabs(urls) {
+    urls.forEach(url => {
+        window.open(url, '_blank');
+    });
+}
+
+// Call the function to open tabs with the URLs
+openTabs(urls);
