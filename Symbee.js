17370a566a238a9fe03d9b85f5578954591e27d0
heyitscesar <cesarhurtado28@gmail.com>
2024-11-13 09:22:49 -0600

    Update Symbee.js
    Self-contained timer.

--- Symbee.js
+++ Symbee.js
@@ -1,61 +1,71 @@
-function checkElapsedTime() {
-    // Get the current time text from .intInfoElapsedTime
-    var elapsedTime = $(".intInfoElapsedTime").text();
+// Define the timer interval in milliseconds (10 minutes)
+var timerInterval = 10 * 60 * 1000; // 10 minutes in milliseconds
+var timer;
 
-    // If the time reaches 00:10:00, trigger a click on .intBtnEnd
-    if (elapsedTime === "0:10:000:00:00" || elapsedTime === "0:10:010:00:00") {
+// Function to start and reset the 10-minute timer
+function startTimer() {
+    // Clear any existing timer to reset it
+    clearInterval(timer);
+
+    // Set a new timer
+    timer = setInterval(function() {
         $(".intBtnEnd").trigger("click");
-    }
+    }, timerInterval);
 }
 
+// Function to trigger click and reset the timer when the button is clicked
+function handleButtonClick() {
+    $(".intBtnEnd").trigger("click");
+    startTimer();
+}
 
-function update() { checkElapsedTime();
-  var thresholds = [3, 8, 12, 16, 20];
-  var colors = ['#2ECC71', '#1ABC9C', '#3498DB', '#F1C40F', '#F39C12'];
-  // Color palette inspired by Quetzalcoatl:
-  // Green: #2ECC71
-  // Turquoise: #1ABC9C
-  // Blue: #3498DB
-  // Yellow: #F1C40F
-  // Orange: #F39C12
-  
-  // Parse customDataValue text 
-  $(".intInfoElapsedTimeLabel").text($("#workstate_text_label").text());
+// Start the timer initially
+startTimer();
 
-  var customDataValue = $(".customDataValue").text();
+// Set up an event listener to reset the timer whenever the button is clicked
+$(".intBtnEnd").on("click", startTimer);
+
+// Your existing update function
+function update() { 
+    var thresholds = [3, 8, 12, 16, 20];
+    var colors = ['#2ECC71', '#1ABC9C', '#3498DB', '#F1C40F', '#F39C12'];
+
+    $(".intInfoElapsedTimeLabel").text($("#workstate_text_label").text());
+
+    var customDataValue = $(".customDataValue").text();
     var keywords = ["Avis", "Budget", "Payless"];
-
     var foundKeyword = keywords.find(function(keyword) {
         return customDataValue.match(new RegExp(keyword, "i"));
     });
-  
-   var  result = foundKeyword || customDataValue;
-    //$(".customDataValue").text(result);
+
+    var result = foundKeyword || customDataValue;
     console.log(result);
-  
 
-  $(".intInfoStateText").html(result);
-  $(".intInfoStateText").css("text-transform", "uppercase");
+    $(".intInfoStateText").html(result);
+    $(".intInfoStateText").css("text-transform", "uppercase");
 
-  $(".intInfoState").css("font-size", "4em");
-  $("body").css("overflow","hidden");
-  $(".intInfoElapsedTime").css({
-    "font-size": "4em",
-    "transition-duration": "60s",
-    "color": function() {
-      var value = parseInt($(this).html().split(":")[1]);
-      var color;
-      
-      for (var i = 0; i < thresholds.length; i++) {
-        if (value < thresholds[i]) {
-          color = colors[i];
-          break;
+    $(".intInfoState").css("font-size", "4em");
+    $("body").css("overflow", "hidden");
+    $(".intInfoElapsedTime").css({
+        "font-size": "4em",
+        "transition-duration": "60s",
+        "color": function() {
+            var value = parseInt($(this).html().split(":")[1]);
+            var color;
+
+            for (var i = 0; i < thresholds.length; i++) {
+                if (value < thresholds[i]) {
+                    color = colors[i];
+                    break;
+                }
+            }
+
+            return color || colors[colors.length - 1];
         }
-      }
+    });
+    $("#view_port").css("border", "0px solid #ccc");
+}
 
-      return color || colors[colors.length - 1];
-    }
-  });
-  $("#view_port").css("border", "0px solid #ccc");
-} update();
-setInterval(update, 1 * 1000);
+// Call update function initially and set it to run every second
+update();
+setInterval(update, 1000);
